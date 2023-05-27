427c6ea9dd540c958bd786d9804b3154ecc6a855
hafeezsolutionriver <hafeez.solutionriver@gmail.com>
2023-05-27 17:56:28 +0500

    Add Thank you Message

--- src/components/AnotherUserParticipation.js
+++ src/components/AnotherUserParticipation.js
@@ -1,14 +1,17 @@
+import ThankYouMessage from "./ThankYouMessage";
+
 function AnotherUserParticipation({setStart}) {
 
     return (<>
-    
-    
+ 
     <button onClick={()=>{
 
         setStart(false)
 
     }}>Particiapte Again</button>
     </>  );
+
+    
 }
 
 export default AnotherUserParticipation;
---
+++ src/components/ThankYouMessage.js
@@ -0,0 +1,5 @@
+function ThankYouMessage() {
+    return ( <h1>Thank You</h1> );
+}
+
+export default ThankYouMessage;
